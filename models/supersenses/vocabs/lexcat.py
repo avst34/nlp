3d823a202153aefc59b5b9f6a30d2ed2c28edbf0
Aviram <aviram@phrasetech.com>
2018-02-12 13:30:40 +0200

    Merge branch 'master' of https://github.com/avst34/nlp
    # Conflicts:
    #	hyperparameters_tuner.py
    #	models/supersenses/lstm_mlp_supersenses_model_hyperparameters_tuner.py
    #	models/supersenses/tuner_domains.py

---
+++ models/supersenses/vocabs/lexcat.py
@@ -0,0 +1,28 @@
+from vocabulary import Vocabulary
+            
+LEXCAT = Vocabulary('LEXCAT', [
+'!!@',
+'N',
+'AUX',
+'PRON',
+'ADV',
+'X',
+'CCONJ',
+'!@',
+'ADJ',
+'INF.P',
+None,
+'NUM',
+'INTJ',
+'V',
+'PRON.POSS',
+'DET',
+'INF',
+'SYM',
+'PP',
+'DISC',
+'P',
+'PUNCT',
+'SCONJ',
+'POSS']
+)
