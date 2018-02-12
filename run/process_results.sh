b7c5d135d2d1ec7c257cf6f592c467b756b05c6f
Aviram <aviram@phrasetech.com>
2018-02-12 13:18:56 +0200

    Merge branch 'master' of https://github.com/avst34/nlp
    # Conflicts:
    #	hyperparameters_tuner.py
    #	models/supersenses/lstm_mlp_supersenses_model_hyperparameters_tuner.py
    #	models/supersenses/tuner_domains.py

--- run/process_results.sh
+++ run/process_results.sh
@@ -6,4 +6,4 @@
 
 source /cs/usr/aviramstern/lab/venvs/local/bin/activate
 
-python3 /cs/usr/aviramstern/nlp_prod/process_tuner_results.py /cs/usr/lab/results.csv
+python3 /cs/usr/aviramstern/nlp_prod/run/process_tuner_results.py /cs/usr/lab/results.csv
