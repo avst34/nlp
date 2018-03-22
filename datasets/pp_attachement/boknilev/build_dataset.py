10c5865befe104f514357d669c611d267c91438a
avst34 <aviram.stern@gmail.com>
2018-03-22 16:41:16 +0200

    supersenses

--- datasets/pp_attachement/boknilev/build_dataset.py
+++ datasets/pp_attachement/boknilev/build_dataset.py
@@ -32,6 +32,11 @@ def read_mrg(mrg_path):
         }
         for sent in processed_sents
     ]
+    for sent in processed_sents:
+        children = []
+        for tok_ind, tok in enumerate(sent['sent']):
+            children.append([i + 1 for i, head in enumerate(sent['head']) if head == tok_ind + 1])
+        sent['children'] = children
     return processed_sents
 
 
@@ -115,11 +120,10 @@ def match_sentence_annotations(sent, anns):
     return matches
 
 
-def match_annotations(annotations, sents, cur_matches=None):
+def match_annotations(annotations, sents):
+    ann_id_to_ann = {ann['id']: ann for ann in annotations}
     match_counts = []
-    matches = cur_matches or {}
-    # match_cands = {ann: [tuple(match)] for ann, match in matches.items()}
-    match_cands = {}
+    mismatch_reason = {}
     sents = copy.deepcopy(sents)
     for sent in sents:
         sent['word_next_pos'] = [(sent['sent'][ind], sent['pos'][ind + 1]) for ind, _ in enumerate(sent['sent'][:-1])]
@@ -127,57 +131,79 @@ def match_annotations(annotations, sents, cur_matches=None):
         sent['word_head'] = [(sent['sent'][ind], sent['head'][ind]) for ind, _ in enumerate(sent['sent'][:-1])]
         sent['word_head_lc'] = [(sent['sent'][ind].lower(), sent['head'][ind]) for ind, _ in enumerate(sent['sent'][:-1])]
         sent['sent_lc'] = [x.lower() for x in sent['sent']]
-    for ann_ind, ann in enumerate(annotations):
-        if ann['id'] in match_cands:
-            continue
-        match_cands[ann['id']] = []
-        for filter_label in [True, False]:
-            for sent_key, wnp_key, wh_key in [('sent', 'word_next_pos', 'word_head'), ('sent_lc', 'word_next_pos_lc', 'word_head_lc')]:
-                for sent in sents:
-                    # if 'wsj.23' in ann['id'] and 'wsj.23' not in sent['id']:
-                    #     continue
-                    # if 'wsj.2-21' in ann['id'] and 'wsj.2-21' not in sent['id']:
-                    #     continue
-                    if ann['children.words'][0] not in sent[sent_key]:
-                        continue
 
-                    for ind, (word, head) in enumerate(sent[wh_key]):
-                        if word == ann['preps.words'][0] and \
-                          (not(filter_label) or 0 <= (head - 1) and
-                            sent[sent_key][head - 1] == ann['heads.words'][int(ann['labels'][0]) - 1]):
-                                window = sent[sent_key][max(ind - 10, 0):ind]
-                                found = True
-                                for h in ann['heads.words']:
-                                    try:
-                                        window = window[window.index(h) + 1:]
-                                    except ValueError:
-                                        found = False
-                                if not found:
-                                    continue
-                                window = sent[wnp_key][max(ind - 10, 0):ind]
-                                found = True
-                                for wnp in zip(ann['heads.words'], ann['heads.next.pos']):
-                                    try:
-                                        window = window[window.index(wnp) + 1:]
-                                    except ValueError:
-                                        found = False
-                                if not found:
-                                    continue
-                                match_cands[ann['id']].append((sent['id'], ind, filter_label))
+    try:
+        with open(os.path.dirname(__file__) + '/data/pp-data-english/wsj_cache.json', 'r') as f:
+            match_cands = {k: [tuple(vv) for vv in v]  for k, v in json.load(f).items()}
+            match_cands_orig = copy.deepcopy(match_cands)
+    except:
+        match_cands = {}
+        match_cands_orig = {}
+
+    for ann_ind, ann in enumerate(annotations):
+        if ann['id'] not in match_cands:
+            match_cands[ann['id']] = []
+            for filter_label in [True, False]:
+                for sent_key, wnp_key, wh_key in [('sent', 'word_next_pos', 'word_head'), ('sent_lc', 'word_next_pos_lc', 'word_head_lc')]:
+                    for sent in sents:
+                        # if 'wsj.23' in ann['id'] and 'wsj.23' not in sent['id']:
+                        #     continue
+                        # if 'wsj.2-21' in ann['id'] and 'wsj.2-21' not in sent['id']:
+                        #     continue
+                        if ann['children.words'][0] not in sent[sent_key]:
+                            continue
+
+                        for ind, (word, head) in enumerate(sent[wh_key]):
+                            if word == ann['preps.words'][0] and \
+                              (not(filter_label) or 0 <= (head - 1) and
+                                sent[sent_key][head - 1] == ann['heads.words'][int(ann['labels'][0]) - 1]):
+                                    window = sent[sent_key][max(ind - 10, 0):ind]
+                                    found = True
+                                    for h in ann['heads.words']:
+                                        try:
+                                            window = window[window.index(h) + 1:]
+                                        except ValueError:
+                                            found = False
+                                    if not found:
+                                        continue
+                                    window = sent[wnp_key][max(ind - 10, 0):ind]
+                                    found = True
+                                    for wnp in zip(ann['heads.words'], ann['heads.next.pos']):
+                                        try:
+                                            window = window[window.index(wnp) + 1:]
+                                        except ValueError:
+                                            found = False
+                                    if not found:
+                                        continue
+                                    match_cands[ann['id']].append((sent['id'], ind, filter_label))
+                    if match_cands[ann['id']]:
+                        break
                 if match_cands[ann['id']]:
                     break
-            if match_cands[ann['id']]:
-                break
 
         if not match_cands[ann['id']]:
             print(ann)
+            mismatch_reason[ann['id']] = 'No sentences matched'
 
         match_counts.append(len(match_cands[ann['id']]))
         print("%d/%d" % (ann_ind, len(annotations)))
 
+    with open(os.path.dirname(__file__) + '/data/pp-data-english/wsj_cache.json', 'w') as f:
+        json.dump(match_cands, f)
+
+    for ann_id, cands in match_cands.items():
+        if cands and not cands[0][2]:
+            print("bad label", ann_id_to_ann[ann_id])
+            if len(cands) > 1:
+                mismatch_reason[ann_id] = 'Only matched sentences with incorrect labels'
+                match_cands[ann_id] = []
+
+    for ann_id, cands in match_cands.items():
+        match_cands[ann_id] = [(x[0], x[1]) for x in cands]
+
+
+
     print(Counter(match_counts))
-    sent_id_to_ind = {s['id']: ind for ind, s in enumerate(sents)}
-    ann_id_to_ind = {ann['id']: ind for ind, ann in enumerate(annotations)}
 
     # missing = [ann_id for ann_id in match_cands if ann_id not in matches]
     # print(sorted(missing))
@@ -190,22 +216,155 @@ def match_annotations(annotations, sents, cur_matches=None):
     matches_per_sent_pp = {}
     for ann_id, ann_matches in match_cands.items():
         for match in ann_matches:
-            print(match)
             matches_per_sent_pp[match] = matches_per_sent_pp.get(match) or []
-            matches_per_sent_pp[match].append(ann_id)
+            matches_per_sent_pp[match].append(ann_id_to_ann[ann_id])
 
-    closed_matches = {ann_ids[0]: match for match, ann_ids in matches_per_sent_pp.items() if len(ann_ids) == 1 and match[2]}
-    closed_matches_missed_label = {ann_ids[0]: match for match, ann_ids in matches_per_sent_pp.items() if len(ann_ids) == 1 and not match[2]}
+    print("Matches: %d Anns: %d" % (len(matches_per_sent_pp), len(annotations)))
 
-    for ann_id, (sent_id, tok_ind, matched_label) in closed_matches.items():
-        ann = annotations[ann_id_to_ind[ann_id]]
+
+
+    def m():
+        print('Clearing by single..')
+        i = 0
+        while True:
+            took_action = False
+            closed_matches = {ann_id: matches[0] for ann_id, matches in match_cands.items() if len(matches) == 1}
+            for ann_id, match in list(closed_matches.items()):
+                if ann_id not in closed_matches:
+                    continue
+                for ann in list(matches_per_sent_pp[match]):
+                    if ann['id'] != ann_id:
+                        took_action = True
+                        assert match in match_cands[ann['id']]
+                        match_cands[ann['id']].remove(match)
+                        if closed_matches.get(ann['id']) == match:
+                            del closed_matches[ann['id']]
+                        matches_per_sent_pp[match].remove(ann)
+                        if not match_cands[ann['id']]:
+                            print(i, "Matches cleared due to a better fit (single option)", ann['id'])
+                            mismatch_reason[ann['id']] = "Matches cleared due to a better fit (single option)"
+            print("plen", len(closed_matches))
+            if not took_action:
+                break
+            i += 1
+
+    m()
+
+    for match, anns in matches_per_sent_pp.items():
+        anns.sort(key=lambda ann:-len(ann['heads.words']))
+        longest = [ann for ann in anns if len(ann['heads.words']) == len(anns[0]['heads.words'])]
+        for ann in list(anns):
+            if ann not in longest:
+                match_cands[ann['id']].remove(match)
+                matches_per_sent_pp[match].remove(ann)
+                if not match_cands[ann['id']]:
+                    mismatch_reason[ann['id']] = "Matches cleared due to a better fit (more heads)"
+
+    m()
+
+    closed_matches = {ann_id: matches[0] for ann_id, matches in match_cands.items() if len(matches) == 1}
+
+    # closed_matches_missed_label = {anns[0]['id']: match for match, anns in matches_per_sent_pp.items() if len(anns) == 1 and not match[2]}
+
+    new_annotations = []
+
+    handled = {}
+    for ann_id in list(match_cands):
+        if ann_id in closed_matches or ann_id in mismatch_reason:
+            continue
+
+        ann_matches = match_cands[ann_id]
+        assert len(ann_matches) > 1
+
+        all_anns = [ann for match in ann_matches for ann in matches_per_sent_pp[match]]
+        all_anns = [ann for ind, ann in enumerate(all_anns) if all_anns.index(ann) == ind]
+        all_matches = ann_matches
+
+        found_inconsistency = False
+        for ann1 in all_anns:
+            for ann2 in all_anns:
+                if ann1['heads.words'] != ann2['heads.words'] or \
+                   ann1['preps.words'] != ann2['preps.words'] or \
+                   ann1['labels'] != ann2['labels']:
+                    found_inconsistency = True
+                    print(ann1)
+                    print(ann2)
+                    break
+            if found_inconsistency:
+                break
+
+        handled.update({ann['id']: True for ann in all_anns})
+        if found_inconsistency:
+            print('WARNING: inconsistency found, skipping %d anns' % len(all_anns))
+            for ann in all_anns:
+                mismatch_reason[ann['id']] = 'Found inconsistency'
+            continue
+
+        if len(all_anns) == len(all_matches):
+            print("Found matching ann-match sets of size", len(all_anns))
+            for mann, mmatch in zip(all_anns, all_matches):
+                match_cands[mann['id']] = [mmatch]
+        elif len(all_anns) > len(all_matches):
+            print("More anns than matches! %d > %d" % (len(all_anns), len(all_matches)))
+            for ann in all_anns:
+                mismatch_reason[ann['id']] = 'More anns than matches'
+        else:
+            print("Less anns than matches! %d < %d" % (len(all_anns), len(all_matches)))
+            for i in range(len(all_matches) - len(all_anns)):
+                nann = copy.deepcopy(all_anns[0])
+                nann['copy_of'] = nann['id']
+                nann['id'] = nann['id'] + '_' + str(i)
+                new_annotations.append(nann)
+                ann_id_to_ann[nann['id']] = nann
+                all_anns.append(nann)
+                for match in all_matches:
+                    matches_per_sent_pp[match].append(nann)
+            for mann, mmatch in zip(all_anns, all_matches):
+                match_cands[mann['id']] = [mmatch]
+        closed_matches.update({ann_id: match_cands[ann_id][0] for ann in all_anns for ann_id in [ann['id']] if len(match_cands[ann_id]) == 1})
+
+    print("Matched %d/%d+%d" % (len(closed_matches), len(annotations), len(new_annotations)))
+    # print("Matched (missed label) %d/%d" % (len(closed_matches_missed_label), len(annotations)))
+    # print("Matched (total) %d/%d" % (len(closed_matches_missed_label) + len(closed_matches), len(annotations)))
+    print("Unmatched: %d" % (len(annotations) + len(new_annotations) - len(closed_matches)))
+
+    print("With at least 1 match: %d" % len([ann_id for ann_id, cands in match_cands.items() if cands]))
+    print("Mismatched with reason: %d" % len(mismatch_reason))
+    print(Counter(mismatch_reason.values()))
+
+    print("Matches: %d Anns: %d" % (len([x for x in matches_per_sent_pp.values() if x]), len(annotations)))
+
+    closed_matches_per_sent_pp = {}
+    for ann_id, match in closed_matches.items():
+        closed_matches_per_sent_pp[match] = closed_matches_per_sent_pp.get(match) or []
+        closed_matches_per_sent_pp[match].append(ann_id_to_ann[ann_id])
+
+    print("Per pp:", Counter([len(x) for x in closed_matches_per_sent_pp.values()]))
+
+    # missing = [ann_id_to_ann[ann_id] for ann_id in match_cands if ann_id not in closed_matches]
+    # orig_matches_per_sent_pp = {}
+    # for ann_id, ann_matches in match_cands_orig.items():
+    #     for match in ann_matches:
+    #         orig_matches_per_sent_pp[(match[0], match[1])] = orig_matches_per_sent_pp.get((match[0], match[1])) or []
+    #         orig_matches_per_sent_pp[(match[0], match[1])].append(ann_id_to_ann[ann_id])
+
+    for ann_id, reason in mismatch_reason.items():
+        if reason == 'No sentences matched':
+            print(ann_id)
+
+    # for ann in missing:
+    #     cands = [(x[0], x[1]) for x in match_cands_orig[ann['id']]]
+    #     available = [c for c in cands if c not in closed_matches_per_sent_pp]
+    #     print("%d/%d available for %s (%s)" % (len(available), len(cands), ann['id'], mismatch_reason[ann['id']]))
+    #     if len(available) == 1 and len(cands) == 1 and mismatch_reason[ann['id']] == 'Matches cleared due to a better fit (single option)':
+    #         m = available[0]
+    #         print(m, 'matched:', [(x['id'], mismatch_reason.get(x['id'])) for x in orig_matches_per_sent_pp[m]])
+    #
+    for ann_id, (sent_id, tok_ind) in closed_matches.items():
+        ann = ann_id_to_ann[ann_id]
         ann['sent_id'] = sent_id
         ann['tok_ind'] = tok_ind
-        ann['matched_label'] = matched_label
 
-    print("Matched (w.label) %d/%d" % (len(closed_matches), len(annotations)))
-    print("Matched (missed label) %d/%d" % (len(closed_matches_missed_label), len(annotations)))
-    print("Matched (total) %d/%d" % (len(closed_matches_missed_label) + len(closed_matches), len(annotations)))
     return closed_matches
 
 
@@ -283,7 +442,8 @@ if __name__ == '__main__':
                 matches = json.load(f)
         except:
             matches = {}
-        matches = match_annotations(annotations, sents, matches)
+
+        matches = match_annotations(annotations, sents)
         with open(os.path.dirname(__file__) + '/data/pp-data-english/wsj_matches.json', 'w') as f:
             json.dump(matches, f)
 
