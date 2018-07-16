e5e50b0da3c983a2301fcd57c197545e59d9752e
Aviram <aviram@phrasetech.com>
2018-07-16 09:39:58 +0300

    Merge branch 'master' of https://github.com/avst34/nlp
    # Conflicts:
    #	hyperparameters_tuner.py
    #	models/supersenses/lstm_mlp_supersenses_model_hyperparameters_tuner.py
    #	models/supersenses/tuner_domains.py

---
+++ datasets/streusle_v4/streusle_dists.py
@@ -0,0 +1,39 @@
+from datasets.streusle_v4 import StreusleLoader
+import os
+from collections import Counter
+from pprint import pprint
+
+loader = StreusleLoader()
+STREUSLE_BASE = os.path.join(os.path.dirname(__file__), 'release')
+task ='goldid.goldsyn'
+train_records = loader.load(STREUSLE_BASE + '/train/streusle.ud_train.' + task + '.json', input_format='json')
+dev_records = loader.load(STREUSLE_BASE + '/dev/streusle.ud_dev.' + task + '.json', input_format='json')
+test_records = loader.load(STREUSLE_BASE + '/test/streusle.ud_test.' + task + '.json', input_format='json')
+records = train_records + dev_records + test_records
+
+samples = [] # (prep, role, func)
+
+for rec in records:
+    for ttok in rec.tagged_tokens:
+        if ttok.supersense_role:
+            assert ttok.supersense_func
+            prep = ' '.join([rec.get_tok_by_ud_id(udid).token for udid in ttok.we_toknums]).lower()
+            samples.append((prep, ttok.supersense_role, ttok.supersense_func))
+
+prep_dist = Counter([x[0] for x in samples])
+role_dist = Counter([x[1] for x in samples])
+func_dist = Counter([x[2] for x in samples])
+pair_dist = Counter([(x[1], x[2]) for x in samples])
+
+print('Prepositions distribution:')
+pprint(prep_dist)
+print('')
+print('Role distribution:')
+pprint(role_dist)
+print('')
+print('Func distribution:')
+pprint(func_dist)
+print('')
+print('Pair distribution:')
+pprint(pair_dist)
+print('')
--- run/tune_simple_supersenses.py
+++ run/tune_simple_supersenses.py
@@ -1,7 +1,7 @@
 import sys
 
 from datasets.streusle_v4 import StreusleLoader
-from models.supersenses_simple.settings import GOV_FUNC, OBJ_FUNC, OBJ_ROLE, GOV_ROLE
+from models.supersenses_simple.settings import GOV_FUNC, OBJ_FUNC, OBJ_ROLE, GOV_ROLE, PREP_FUNC, PREP_ROLE
 from models.supersenses_simple.simple_mlp_supersenses_model_hyperparameters_tuner import \
     SimpleSupersensesModelHyperparametersTuner
 from models.supersenses_simple.streusle_integration import streusle_record_to_simple_lstm_model_samples
@@ -15,10 +15,8 @@ train_samples = [s for r in train_recs for s in streusle_record_to_simple_lstm_m
 dev_samples = [s for r in dev_recs for s in streusle_record_to_simple_lstm_model_samples(r)]
 
 tasks = {
-    'OBJ_FUNC': OBJ_FUNC,
-    'GOV_FUNC': GOV_FUNC,
-    'OBJ_ROLE': OBJ_ROLE,
-    'GOV_ROLE': GOV_ROLE,
+    'PREP_FUNC': PREP_FUNC,
+    'PREP_ROLE': PREP_ROLE
 }
 
 print("Tuning..")
