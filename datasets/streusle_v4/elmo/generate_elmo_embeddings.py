be432913e91684d67db119ff3462179acaed10bf
avst34 <aviram.stern@gmail.com>
2018-08-01 15:37:29 +0300

    elmo

--- datasets/streusle_v4/elmo/generate_elmo_embeddings.py
+++ datasets/streusle_v4/elmo/generate_elmo_embeddings.py
@@ -1,6 +1,6 @@
 from datasets.streusle_v4 import StreusleLoader
 
-records = StreusleLoader().load()
+# records = StreusleLoader().load()
 
 def gen_sents_file():
     sents = [' '.join([t.token for t in rec.tagged_tokens]) + '\n' for rec in records]
@@ -8,3 +8,9 @@ def gen_sents_file():
         for sent in sents:
             f.write(sent)
 
+records_with_elmo = StreusleLoader(load_elmo=True).load()
+StreusleLoader(load_elmo=True).load_train()
+StreusleLoader(load_elmo=True).load_dev()
+StreusleLoader(load_elmo=True).load_test()
+for rec in records_with_elmo[:2]:
+    print(rec.tagged_tokens[0].elmo)
