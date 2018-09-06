45c8d0aab8656a2c5274291900086dec61d87a13
avst34 <aviram.stern@gmail.com>
2018-09-06 16:29:02 +0300

    pairwise clustering

---
+++ run/launch_tune_pairwise_clust.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#SBATCH --mem=4096m
+#SBATCH -c2
+#SBATCH --time=2-0
+##SBATCH --gres=gpu:1
+
+source /cs/usr/aviramstern/lab/venvs/$1/bin/activate
+
+SRC_BASE=/cs/usr/aviramstern/lab/nlp_prod
+
+# DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+export PYTHONPATH=$SRC_BASE
+while true; do
+    #    #python3 $DIR/../run/tune_hcpd_model.py > ~/baseline.out
+    # python3 $DIR/../main.py --dynet-gpu --dynet-autobatch > ~/baseline.out
+    export RESULTS_PATH=$SRC_BASE/../results_pairwise.csv
+    python3 -u $SRC_BASE/run/tune_pairwise_func_clust_model.py $SRC_BASE/../results_pairwise.csv
+done
