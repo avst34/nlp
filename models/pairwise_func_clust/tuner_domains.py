502b6d6c0ca8e5310b4cc7126f0af0e3e2e62981
avst34 <aviram.stern@gmail.com>
2018-09-06 16:32:13 +0300

    pairwise clustering

--- models/pairwise_func_clust/tuner_domains.py
+++ models/pairwise_func_clust/tuner_domains.py
@@ -21,7 +21,7 @@ TUNER_DOMAINS = [
     PS(name='num_lstm_layers', values=[2]),
     PS(name='is_bilstm', values=[True]),
     PS(name='lstm_dropout_p', values=np.arange(.51, step=.01)),
-    PS(name='epochs', values=[2]),
+    PS(name='epochs', values=[100]),
     PS(name='learning_rate', values=np.logspace(-2, -1, 6)),
     PS(name='learning_rate_decay', values=np.r_[0, np.logspace(-5, -1, 9)]),
     PS(name='dynet_random_seed', values=[os.environ.get('DYNET_RANDOM_SEED')], enabled=True)
