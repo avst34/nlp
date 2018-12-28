136b0afdb6f2552481bf2d3e7b758def7319d5e9
Aviram <aviram@phrasetech.com>
2018-12-28 17:19:44 +0200

    Merge branch 'master' of https://github.com/avst34/nlp
    # Conflicts:
    #	hyperparameters_tuner.py
    #	models/supersenses/lstm_mlp_supersenses_model_hyperparameters_tuner.py
    #	models/supersenses/tuner_domains.py

---
+++ run/eval_lp_eng.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#SBATCH --mem=4096m
+#SBATCH -c2
+#SBATCH --time=2-0
+##SBATCH --gres=gpu:1
+
+source /cs/usr/aviramstern/lab/venvs/$1/bin/activate
+
+SRC_BASE=/cs/usr/aviramstern/lab/nlp_prod
+
+# DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+export PYTHONPATH=$SRC_BASE
+while true; do
+    export RESULTS_PATH=$SRC_BASE/../muse_chinese_results.csv
+    python3 -u $SRC_BASE/run/muse_eval_chinese.py $SRC_BASE/../muse_chinese_results.csv
+done
