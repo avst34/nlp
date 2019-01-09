17167320053c3b1ba46c8c1f9f81faf7ee3dad05
avst34 <aviram.stern@gmail.com>
2019-01-09 13:20:39 +0200

    standalone

--- models/supersenses/embeddings/__init__.py
+++ models/supersenses/embeddings/__init__.py
@@ -1,10 +1,6 @@
 from .boolean import BOOLEAN
 from .fasttext_en import FASTTEXT_EN
 from .lemmas_word2vec import LEMMAS_WORD2VEC
-from .muse_en import MUSE_EN
-from .muse_streusle import MUSE_STREUSLE
-from .muse_streusle_dict import MUSE_STREUSLE_DICT
-from .muse_zh import MUSE_ZH
 from .tokens_word2vec import TOKENS_WORD2VEC
 
 AUTO = 'AUTO' # A randomally initialized embeddings that will be leared during training (vector size should be provided extenally)
--- models/supersenses/embeddings/muse_en_streusle.py
+++
@@ -1,3 +0,0 @@
-from embeddings.embeddings_hdf5 import EmbeddingsHDF5Reader
-
-MUSE_EN = EmbeddingsHDF5Reader('/cs/usr/aviramstern/lab/muse/embeddings/vectors-en-streusle.hd5')
