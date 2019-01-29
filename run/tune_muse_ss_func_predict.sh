f4bd80c137d86a7376ea06a009a1a9ec48ad21ee
Aviram <aviram@phrasetech.com>
2019-01-29 07:44:27 +0200

    Merge branches 'master' and 'pss-cli' of https://github.com/avst34/nlp
    # Conflicts:
    #	models/supersenses/embeddings/muse_streusle_dict.py

--- run/tune_muse_ss_func_predict.sh
+++ run/tune_muse_ss_func_predict.sh
@@ -11,6 +11,6 @@ SRC_BASE=/cs/usr/aviramstern/lab/nlp_prod
 # DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export PYTHONPATH=$SRC_BASE
 while true; do
-    export RESULTS_PATH=$SRC_BASE/../muse_pss_func_predict_full_model_results.csv
+    export RESULTS_PATH=$SRC_BASE/../muse_pss_func_predict_full_model_results_elmo.csv
     $2 -u $SRC_BASE/run/muse_pss_func_predict.py $SRC_BASE/../muse_pss_func_predict_full_model_results_elmo.csv
 done
