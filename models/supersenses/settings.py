bb5471016d029dbfc22a3cd54389bab37883884d
avst34 <aviram.stern@gmail.com>
2019-03-13 13:52:29 +0200

    fix, adam

--- models/supersenses/settings.py
+++ models/supersenses/settings.py
@@ -13,12 +13,12 @@ GOLD_ID_AUTO_PREP = override_settings([TUNER_DOMAINS, [
 ]])
 
 AUTO_ID_GOLD_PREP = override_settings([TUNER_DOMAINS, [
-    PS(name='allow_empty_prediction', values=[True]),
+    PS(name='allow_empty_prediction', values=[False]),
     PS(name='embd_type', values=['fasttext_en', 'elmo'])
 ]])
 
 AUTO_ID_AUTO_PREP = override_settings([TUNER_DOMAINS, [
-    PS(name='allow_empty_prediction', values=[True]),
+    PS(name='allow_empty_prediction', values=[False]),
     PS(name='embd_type', values=['fasttext_en', 'elmo'])
 ]])
 
@@ -125,3 +125,27 @@ ELMO_FASTTEXT_MIN_TASK_SETTINGS = {
     ])
     for task, settings in ELMO_FASTTEXT_TASK_SETTINGS.items()
 }
+
+ELMO_MIN_NELSON = override_settings([
+    ELMO_FASTTEXT_MIN_TASK_SETTINGS['goldid.goldsyn'],
+    {
+        PS(name='embd_type', values=['elmo']),
+        PS(name='elmo_layer', values=[1]),
+        PS(name='trainer', values=['adam']),
+        PS(name='learning_rate', values=['0.001']),
+        PS(name='use_lemma', values=[False]),
+        PS(name='use_capitalized_word_follows', values=[False]),
+        PS(name='use_parent', values=[False]),
+        PS(name='use_grandparent', values=[False]),
+        PS(name='use_token_internal', values=[False]),
+        PS(name='use_prep', values=[True]),
+        PS(name='use_ud_xpos', values=[False]),
+        PS(name='use_ud_dep', values=[False]),
+        PS(name='use_ner', values=[False]),
+        PS(name='use_govobj', values=[False]),
+        PS(name='use_lexcat', values=[False]),
+        PS(name='update_token_embd', values=[False]),
+        PS(name='epochs', values=[130]),
+
+    }
+])
