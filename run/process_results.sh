450d2d85aaf474c73f488fea7dceef6b45dc873c
Aviram <aviram@phrasetech.com>
2019-03-17 16:43:05 +0200

    Merge branches 'master' and 'pss-cli' of https://github.com/avst34/nlp
    # Conflicts:
    #	models/supersenses/embeddings/muse_streusle_dict.py

--- run/process_results.sh
+++ run/process_results.sh
@@ -1,9 +1,4 @@
-#!/bin/sh
-#SBATCH --mem=4096m
-#SBATCH -c2
-#SBATCH --time=2-0
-#SBATCH --gres=gpu:1
-
-source /cs/usr/aviramstern/lab/venvs/local/bin/activate.csh
-
-python3 /cs/usr/aviramstern/nlp_prod/process_tuner_result.py /cs/usr/aviramstern/lab/results.csv
+sbatch /cs/usr/aviramstern/nlp_prod/run/run_process_results.sh /cs/usr/aviramstern/lab/full_model.csv goldid_goldsyn
+sbatch /cs/usr/aviramstern/nlp_prod/run/run_process_results.sh /cs/usr/aviramstern/lab/full_model.csv goldid_autosyn
+sbatch /cs/usr/aviramstern/nlp_prod/run/run_process_results.sh /cs/usr/aviramstern/lab/full_model.csv autoid_goldsyn
+sbatch /cs/usr/aviramstern/nlp_prod/run/run_process_results.sh /cs/usr/aviramstern/lab/full_model.csv autoid_autosyn
