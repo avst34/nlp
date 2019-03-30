b5495fa5572ac41246589bf9ae7bd0e9ee716351
Aviram <aviram@phrasetech.com>
2019-03-30 15:43:48 +0300

    Merge branches 'master' and 'pss-cli' of https://github.com/avst34/nlp
    # Conflicts:
    #	models/supersenses/embeddings/muse_streusle_dict.py

--- run/process_results.sh
+++ run/process_results.sh
@@ -1,9 +1,9 @@
-sbatch /cs/usr/aviramstern/lab/nlp_prod/run/run_process_results.sh /cs/usr/aviramstern/lab/full_model.csv /cs/usr/aviramstern/lab/nlp_prod/run/run_process_results.sh /cs/usr/aviramstern/lab/full_model2.csv goldid.goldsyn all
-sbatch /cs/usr/aviramstern/lab/nlp_prod/run/run_process_results.sh /cs/usr/aviramstern/lab/full_model.csv /cs/usr/aviramstern/lab/nlp_prod/run/run_process_results.sh /cs/usr/aviramstern/lab/full_model2.csv goldid.autosyn all
-sbatch /cs/usr/aviramstern/lab/nlp_prod/run/run_process_results.sh /cs/usr/aviramstern/lab/full_model.csv /cs/usr/aviramstern/lab/nlp_prod/run/run_process_results.sh /cs/usr/aviramstern/lab/full_model2.csv autoid.goldsyn all
-sbatch /cs/usr/aviramstern/lab/nlp_prod/run/run_process_results.sh /cs/usr/aviramstern/lab/full_model.csv /cs/usr/aviramstern/lab/nlp_prod/run/run_process_results.sh /cs/usr/aviramstern/lab/full_model2.csv autoid.autosyn all
+sbatch /cs/usr/aviramstern/lab/nlp_prod/run/run_process_results.sh /cs/usr/aviramstern/lab/full_model.csv /cs/usr/aviramstern/lab/full_model2.csv goldid.goldsyn all
+sbatch /cs/usr/aviramstern/lab/nlp_prod/run/run_process_results.sh /cs/usr/aviramstern/lab/full_model.csv /cs/usr/aviramstern/lab/full_model2.csv goldid.autosyn all
+sbatch /cs/usr/aviramstern/lab/nlp_prod/run/run_process_results.sh /cs/usr/aviramstern/lab/full_model.csv /cs/usr/aviramstern/lab/full_model2.csv autoid.goldsyn all
+sbatch /cs/usr/aviramstern/lab/nlp_prod/run/run_process_results.sh /cs/usr/aviramstern/lab/full_model.csv /cs/usr/aviramstern/lab/full_model2.csv autoid.autosyn all
 
-sbatch /cs/usr/aviramstern/lab/nlp_prod/run/run_process_results.sh /cs/usr/aviramstern/lab/full_model.csv /cs/usr/aviramstern/lab/nlp_prod/run/run_process_results.sh /cs/usr/aviramstern/lab/full_model2.csv goldid.goldsyn elmo_nosyn
-sbatch /cs/usr/aviramstern/lab/nlp_prod/run/run_process_results.sh /cs/usr/aviramstern/lab/full_model.csv /cs/usr/aviramstern/lab/nlp_prod/run/run_process_results.sh /cs/usr/aviramstern/lab/full_model2.csv goldid.goldsyn elmo_syn
-sbatch /cs/usr/aviramstern/lab/nlp_prod/run/run_process_results.sh /cs/usr/aviramstern/lab/full_model.csv /cs/usr/aviramstern/lab/nlp_prod/run/run_process_results.sh /cs/usr/aviramstern/lab/full_model2.csv goldid.goldsyn fasttext_nosyn
-sbatch /cs/usr/aviramstern/lab/nlp_prod/run/run_process_results.sh /cs/usr/aviramstern/lab/full_model.csv /cs/usr/aviramstern/lab/nlp_prod/run/run_process_results.sh /cs/usr/aviramstern/lab/full_model2.csv goldid.goldsyn fasttext_syn
+sbatch /cs/usr/aviramstern/lab/nlp_prod/run/run_process_results.sh /cs/usr/aviramstern/lab/full_model.csv /cs/usr/aviramstern/lab/full_model2.csv goldid.goldsyn elmo_nosyn
+sbatch /cs/usr/aviramstern/lab/nlp_prod/run/run_process_results.sh /cs/usr/aviramstern/lab/full_model.csv /cs/usr/aviramstern/lab/full_model2.csv goldid.goldsyn elmo_syn
+sbatch /cs/usr/aviramstern/lab/nlp_prod/run/run_process_results.sh /cs/usr/aviramstern/lab/full_model.csv /cs/usr/aviramstern/lab/full_model2.csv goldid.goldsyn fasttext_nosyn
+sbatch /cs/usr/aviramstern/lab/nlp_prod/run/run_process_results.sh /cs/usr/aviramstern/lab/full_model.csv /cs/usr/aviramstern/lab/full_model2.csv goldid.goldsyn fasttext_syn
