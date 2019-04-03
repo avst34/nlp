02a54e4809112331617a4f25cd8a85adb60e5094
avst34 <aviram.stern@gmail.com>
2019-04-03 14:33:36 +0300

    elmo

---
+++ run/process_results_local.sh
@@ -0,0 +1,14 @@
+python /cs/usr/aviramstern/lab/nlp/process_tuner_result.py /cs/usr/aviramstern/lab/full_model.csv /cs/usr/aviramstern/lab/full_model2.csv goldid.goldsyn all &
+python /cs/usr/aviramstern/lab/nlp/process_tuner_result.py /cs/usr/aviramstern/lab/full_model.csv /cs/usr/aviramstern/lab/full_model2.csv goldid.autosyn all &
+python /cs/usr/aviramstern/lab/nlp/process_tuner_result.py /cs/usr/aviramstern/lab/full_model.csv /cs/usr/aviramstern/lab/full_model2.csv autoid.goldsyn all &
+python /cs/usr/aviramstern/lab/nlp/process_tuner_result.py /cs/usr/aviramstern/lab/full_model.csv /cs/usr/aviramstern/lab/full_model2.csv autoid.autosyn all &
+
+python /cs/usr/aviramstern/lab/nlp/process_tuner_result.py /cs/usr/aviramstern/lab/full_model.csv /cs/usr/aviramstern/lab/full_model2.csv goldid.goldsyn full_syntax &
+python /cs/usr/aviramstern/lab/nlp/process_tuner_result.py /cs/usr/aviramstern/lab/full_model.csv /cs/usr/aviramstern/lab/full_model2.csv goldid.autosyn full_syntax &
+python /cs/usr/aviramstern/lab/nlp/process_tuner_result.py /cs/usr/aviramstern/lab/full_model.csv /cs/usr/aviramstern/lab/full_model2.csv autoid.goldsyn full_syntax &
+python /cs/usr/aviramstern/lab/nlp/process_tuner_result.py /cs/usr/aviramstern/lab/full_model.csv /cs/usr/aviramstern/lab/full_model2.csv autoid.autosyn full_syntax &
+
+python /cs/usr/aviramstern/lab/nlp/process_tuner_result.py /cs/usr/aviramstern/lab/full_model.csv /cs/usr/aviramstern/lab/full_model2.csv goldid.goldsyn elmo_nosyn &
+python /cs/usr/aviramstern/lab/nlp/process_tuner_result.py /cs/usr/aviramstern/lab/full_model.csv /cs/usr/aviramstern/lab/full_model2.csv goldid.goldsyn elmo_syn &
+python /cs/usr/aviramstern/lab/nlp/process_tuner_result.py /cs/usr/aviramstern/lab/full_model.csv /cs/usr/aviramstern/lab/full_model2.csv goldid.goldsyn fasttext_nosyn &
+python /cs/usr/aviramstern/lab/nlp/process_tuner_result.py /cs/usr/aviramstern/lab/full_model.csv /cs/usr/aviramstern/lab/full_model2.csv goldid.goldsyn fasttext_syn &
