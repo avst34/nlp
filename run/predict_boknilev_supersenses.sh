0445dcf420f6ac6c8f78a76a616b8cf143f4d974
Aviram <aviram@phrasetech.com>
2019-04-09 17:05:19 +0300

    Merge branches 'master' and 'pss-cli' of https://github.com/avst34/nlp
    # Conflicts:
    #	models/supersenses/embeddings/muse_streusle_dict.py

---
+++ run/predict_boknilev_supersenses.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#SBATCH --mem=4096m
+#SBATCH -c2
+#SBATCH --time=2-0
+##SBATCH --gres=gpu:1
+
+#source /cs/usr/aviramstern/lab/venvs/$1/bin/activate
+
+SRC_BASE=/cs/usr/aviramstern/lab/nlp_prod
+
+export PYTHONPATH=$SRC_BASE
+while true; do
+    /cs/usr/aviramstern/lab/python_phoenix/bin/python3 -u  $SRC_BASE/run/predict_boknilev_supersenses.py
+done
