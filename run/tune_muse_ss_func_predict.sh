4b5ac7b46791cc2365e71f0a81e3d5779a0ec012
avst34 <aviram.stern@gmail.com>
2019-05-16 12:16:07 +0300

    hpcd

--- run/tune_muse_ss_func_predict.sh
+++ run/tune_muse_ss_func_predict.sh
@@ -4,13 +4,13 @@
 #SBATCH --time=2-0
 ##SBATCH --gres=gpu:1
 
-source /cs/usr/aviramstern/lab/venvs/$1/bin/activate
+#source /cs/usr/aviramstern/lab/venvs/$1/bin/activate
 
 SRC_BASE=/cs/usr/aviramstern/lab/nlp_prod
 
 # DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export PYTHONPATH=$SRC_BASE
 while true; do
-    export RESULTS_PATH=$SRC_BASE/../muse_pss_func_predict_full_model_results_elmo.csv
-    $2 -u $SRC_BASE/run/muse_pss_func_predict.py $SRC_BASE/../muse_pss_func_predict_full_model_results_elmo.csv
+    export RESULTS_PATH=$SRC_BASE/../chinese_results.csv
+    /cs/usr/aviramstern/lab/python_phoenix/bin/python3 -u $SRC_BASE/run/muse_pss_func_predict.py $SRC_BASE/../chinese_results.csv
 done
