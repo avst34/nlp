98054f32567580bdd3ae5afcfd5f5af5f8587b71
Aviram <aviram@phrasetech.com>
2019-05-22 12:03:31 +0300

    Merge branches 'master' and 'pss-cli' of https://github.com/avst34/nlp
    # Conflicts:
    #	models/supersenses/embeddings/muse_streusle_dict.py

--- docs/render.js
+++ docs/render.js
@@ -1,7 +1,8 @@
 var handlebars = require('handlebars');
 var fs = require('fs');
-var template = fs.readFileSync('./template.handlebars.txt', 'utf8');
-var context = JSON.parse(fs.readFileSync('/cs/labs/oabend/aviramstern/best_results/template_input.json', 'utf8'));
+var template = fs.readFileSync('./ch_template.handlebars.txt', 'utf8');
+// var context = JSON.parse(fs.readFileSync('/cs/labs/oabend/aviramstern/best_results/template_input.json', 'utf8'));
+var context = JSON.parse(fs.readFileSync('/Projects/nlp/docs/chinese_results.json', 'utf8'));
 
 var compiled = handlebars.compile(template);
 var rendered = compiled(context);
